2e4febb22d8d6562a3ef5a3c86c0a521f40f7680
El-Gobbo <177414121+El-Gobbo@users.noreply.github.com>
2025-09-28 23:03:57 +0100

    Add constants for reference

--- blackjack/headers/Blackjack.h
+++ blackjack/headers/Blackjack.h
@@ -7,23 +7,28 @@
 #include "CgCore.h"
 
 namespace Blackjack {
+	constexpr int scoreToWin{ 21 };
+	constexpr int averageCardsAtEndOfGame{ 3 };
+	constexpr std::array<int, CgCore::max_faces> cardScores{ 1,2,3,4,5,6,7,8,9,10,10,10,10 };
+	static_assert(std::size(cardScores) == CgCore::max_faces, "cardScores array not the same length as Faces enum");
+
 	class Hand {
 	public:
 		//Constructors
 		Hand(std::string playerName);
 
 		//Getters
-		const std::string& getPlayerName() const { return m_playerName; }
-		const std::vector<CgCore::Card>& getCards() const { return m_cards; }
-		bool getIsDealer() const { return m_isDealer; }
-		bool getIsStanding() const { return m_isStanding; }
+		const std::string& playerName() const { return m_playerName; }
+		const std::vector<CgCore::Card>& cards() const { return m_cards; }
+		bool isDealer() const { return m_isDealer; }
+		bool isStanding() const { return m_isStanding; }
 
 		//Setters
+		void draw(CgCore::Card card) { m_cards.push_back(card); }
 		void setIsDealer(bool isDealer) { m_isDealer = isDealer; }
 		void setIsBust(bool isBust) { m_isBust = isBust; }
 
 		//Other Functions
-		void draw(CgCore::Card card);
 		std::vector<CgCore::Card> discardHand();
 		bool isBust();
 		int calculateScore();
