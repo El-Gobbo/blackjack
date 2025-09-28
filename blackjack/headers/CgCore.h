7468cd996f96d240955410d25d91b51e91e0894f
El-Gobbo <177414121+El-Gobbo@users.noreply.github.com>
2025-09-28 20:41:18 +0100

    Initial Commit

---
+++ blackjack/headers/CgCore.h
@@ -0,0 +1,62 @@
+#ifndef CGCORE_H
+#define CGCORE_H
+
+#include <array>
+#include <string_view>
+
+namespace CgCore {
+	enum Suit {
+		spades,
+		hearts,
+		diamonds,
+		clubs,
+		max_suits
+	};
+	constexpr std::array<std::string_view, max_suits> suits{
+"spades", "hearts", "diamonds", "clubs"
+	};
+	static_assert(suits.size() == max_suits, "Error: suits array does not match Suit enum count");
+
+	enum Face {
+		ace,
+		two,
+		three,
+		four,
+		five,
+		six,
+		seven,
+		eight,
+		nine,
+		ten,
+		jack,
+		queen,
+		king,
+		max_faces
+	};
+	constexpr std::array<std::string_view, max_faces> faces{
+		"ace", "two", "three", "four", "five",
+		"six", "seven", "eight", "nine", "ten",
+		"jack", "queen", "king"
+	};
+	static_assert(faces.size() == max_faces, "Error: faces array does not match Face enum count");
+
+	constexpr std::size_t deckSize{ 52 };
+
+	class Card {
+	public:
+		Suit suit() { return m_suit; }
+		Face face() { return m_face; }
+	private:
+		Suit m_suit{};
+		Face m_face{};
+	};
+
+	class Deck {
+	public:
+		Deck()
+	private:
+		std::array<Card, deckSize> m_cards{};
+	};
+}
+
+#endif
